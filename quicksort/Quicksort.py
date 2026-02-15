0f9f8027b8e35047b1e96d1f7717477577b5ff89
19floresa <alexander.flores0920@gmail.com>
2026-02-15 13:42:40 -0800

    added more test cases

--- quicksort/Quicksort.py
+++ quicksort/Quicksort.py
@@ -1,4 +1,6 @@
 def quicksort(data, index_start, index_end):
+    if len(data) <= 1:
+        return data
     if (index_start >= 0) and (index_start < index_end):
         (start, end) = partition(data, index_start, index_end)
         data = quicksort(data, index_start, start-1)
@@ -28,5 +30,13 @@ def partition(data, index_start, index_end):
 
     return (lt, gt)
 
-test1 = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
-print(quicksort(test1, 0, len(test1)-1))
+# test1 = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
+# test2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] 
+# test3 = [42]
+# test4 = [4, 2, 4, 1, 4, 3, 4, 5]
+# test5 = [7, 7, 7, 7, 7]
+# print(quicksort(test1, 0, len(test1)-1))
+# print(quicksort(test2, 0, len(test2)-1))
+# print(quicksort(test3, 0, len(test3)-1))
+# print(quicksort(test4, 0, len(test4)-1))
+# print(quicksort(test5, 0, len(test5)-1))
